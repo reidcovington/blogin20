a0ed49e8dd25f7a189c9598ed0722d6a9aa261ce
Reid Covington <reid@shopcube.com>
2014-07-22 12:42:28 -0400

    Add edit view -remove database query from views and put in controller

--- blogin15/app/controllers/posts_controller.rb
+++ blogin15/app/controllers/posts_controller.rb
@@ -5,4 +5,22 @@ class PostsController < ApplicationController
     @posts = Post.order("created_at desc")
     respond_with @posts
   end
+
+  def new
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def create
+    p params
+    @post = Post.create(user_params)
+    redirect_to posts_path
+  end
+
+  def user_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
 end
