094d6a4639b8595979e95869c3886859bf3cf73e
Reid Covington <reid@shopcube.com>
2014-07-23 15:35:50 -0400

    Add destroy

--- blogin14/app/controllers/posts_controller.rb
+++ blogin14/app/controllers/posts_controller.rb
@@ -35,6 +35,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
