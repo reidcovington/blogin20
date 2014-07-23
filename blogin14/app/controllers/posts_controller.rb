221f37ea3a650316ea73ca2b517ff6f601b354af
Reid Covington <reid@shopcube.com>
2014-07-23 15:32:00 -0400

    Add index, new, create, edit, update methods and views

--- blogin14/app/controllers/posts_controller.rb
+++ blogin14/app/controllers/posts_controller.rb
@@ -1,2 +1,41 @@
 class PostsController < ApplicationController
+  respond_to :html
+
+  def index
+    @posts = Post.order('created_at desc')
+    respond_with @posts
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    @post = Post.find_by_id(params[:id])
+    @post.update_attributes(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render "edit"
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
