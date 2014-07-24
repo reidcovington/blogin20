e3869451bf50b27718475f8a4e1c959fb57ce89c
Reid Covington <reid@shopcube.com>
2014-07-24 15:42:25 -0400

    add comment create and destroy method

--- blogin13/app/controllers/comments_controller.rb
+++ blogin13/app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find_by_id(params[:post_id])
+    @post.comments.create(params_comment)
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:post_id])
+    @comment = Comment.find_by_id(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
+  def params_comment
+    params.require(:comment).permit(:author, :body)
+  end
 end
+
